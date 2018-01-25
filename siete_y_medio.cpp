920bc9812d8152923156f792ab93624acad0c140
Kathyhan Nguyen <kathyhan19@gmail.com>
2018-01-25 13:47:45 -0800

    Main code to play 1 round

--- siete_y_medio.cpp
+++ siete_y_medio.cpp
@@ -21,7 +21,7 @@ void print_card(Card A) {
 	string engrank = A.get_english_rank();
 	string engsuit = A.get_english_suit();
 	std::cout << "\t" << spanrank << " de " << spansuit;
-	std::cout << "\t" << "(" << engrank << " of " << engsuit << ") .\n";
+	std::cout << "\t" << "(" << engrank << " of " << engsuit << ").\n";
 }
 
 void print_hand(Hand B) {
@@ -30,10 +30,13 @@ void print_hand(Hand B) {
 		print_card(bcards[i]); 
 	}
 }
-// Stub for main
+
+
 int main() {
-	Player main(100);
+	Player main(100); 
+	Player dealer(900); 
 	Hand main_hand;
+	Hand dealer_hand; 
 
 	//Ask Player to bet 
 	int bet = 0; 
@@ -50,21 +53,41 @@ int main() {
 	print_hand(main_hand); 
 	std::cout << "Your total is " << main_hand.value() << ". "; 
 	
-	/*
 	//Prompting player to draw another card or end their turn
-	std:: cout << "Do you want another card (y/n)? " << std::endl;
-	char x; 
-	std::cin >> x;
-	if (x == 'y')
+	char x = 'y'; 
+	while (x == 'y') {
+		if (main_hand.value() >= 7.5) break;
+		std::cout << "Do you want another card (y/n)? " << std::endl;
+		std::cin >> x;
+		if (x == 'n') break;
+		if (x == 'y')
+		{
+			std::cout << "\nNew card: \n";
+			Card new_card;
+			main_hand.add_card(new_card);
+			print_card(new_card);
+			std::cout << "Your cards: \n";
+			print_hand(main_hand);
+			std::cout << "Your total is " << main_hand.value() << ". ";
+		}
+	}
+
+	//Dealer's Turn 
+	std::cout << "\nDealer's cards: \n"; 
+	print_hand(dealer_hand); 
+	std::cout << "The dealer's total is " << dealer_hand.value() << ". \n"; 
+	while (dealer_hand.value() < 5.5)
 	{
-	std::cout << "New card: \n";
-	Card new_card;
-	main_hand.add_card(new_card); 
-	cout << "\t"; 
-	print_card(new_card); 
-	std::cout << "Your cards: \n"; 
+		std::cout << "\nNew card: \n";
+		Card A;
+		dealer_hand.add_card(A); 
+		print_card(A); 
+		std::cout << "\nDealer's cards: \n"; 
+		print_hand(dealer_hand); 
+		std::cout << "The dealer's total is " << dealer_hand.value() << ". "; 
+	}
+ 
 
 
-	} */
 	return 0;
 }
